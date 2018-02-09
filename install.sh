21a455810222f81e0450eb572f229695fa0910f7
xiaobocser <xiaobocser@qq.com>
2018-02-09 10:25:28 +0800

    install.sh fix empty dirname

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-path=$(dirname `readlink -f $0`)
+path=`readlink -f $0`
+real_path=""
+i=0
+for pt in $path ; do
+	if [ $i == 0 ] ; then
+		real_path=$pt
+		i=1
+	else
+		real_path=$real_path"\ "$pt
+	fi
+done
+path=${real_path%/*}
 export PATH=$PATH:$path
 echo "export PATH=\$PATH":$path >> ~/.bash_profile
 chmod +x wallpaper

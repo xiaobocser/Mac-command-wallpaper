5e0ab340beedf87274e2fcbb3f79c90019c7f66f
FoxerLee <foxerlee1@gmail.com>
2018-04-29 01:16:28 +0800

    fix imagemagick@6 error

--- bin/generate_code_calender.py
+++ bin/generate_code_calender.py
@@ -2,16 +2,42 @@
 import sys
 import datetime
 import os
+import io
 from wand.image import Image
+from wand.color import Color
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+memo = {}
+
+def getPdfReader(filename):
+    reader = memo.get(filename, None)
+    if reader is None:
+        reader = PdfFileReader(filename, strict=False)
+        memo[filename] = reader
+    return reader
 
 
 def composite_image(week_num, pdf_path, pic_path, pic_save_path):
     start_page = 6  # PDF start page
     dis_left = 10
-    dis_top = 8
+    dis_top = 10
+
+    pdfile = getPdfReader(pdf_path)
+    pageObj = pdfile.getPage(week_num)
+    dst_pdf = PdfFileWriter()
+    dst_pdf.addPage(pageObj)
+
+    pdf_bytes = io.BytesIO()
+    dst_pdf.write(pdf_bytes)
+    pdf_bytes.seek(0)
     
     page = start_page + week_num
-    with Image(filename=pdf_path.format(page), resolution=200) as calendar:
+    with Image(file=pdf_bytes, resolution=200) as calendar:
+        calendar.format = 'png'
+        # calendar.background_color = Color("transparent")
+        with Color('#FFFFFF') as white:
+            twenty_percent = int(65535 * 0.2)  # Note: percent must be calculated from Quantum
+            calendar.transparent_color(white, alpha=0.0, fuzz=twenty_percent)
         with Image(filename=pic_path) as background:
             background.composite_channel(
                 'default_channels', calendar, 'blend', dis_left, dis_top)
@@ -20,7 +46,7 @@ def composite_image(week_num, pdf_path, pic_path, pic_save_path):
 
 def main():
     path = os.getcwd()  # now path
-    pdf_path = path + '/source/2018_code_calendar.pdf[{}]'  # pdf path
+    pdf_path = path + '/source/2018_code_calendar.pdf'  # pdf path
     pic_path = path + '/background/yosemiteonfire_2560x1440.jpg'  # wallpaper
     if len(sys.argv) > 1:
         pic_path = sys.argv[1]
